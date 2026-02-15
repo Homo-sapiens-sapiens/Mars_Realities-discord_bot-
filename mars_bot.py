f19514979da820f34b86fcce4bdec098ab3748da
uranium__hexafluoride <139465358+Homo-sapiens-sapiens@users.noreply.github.com>
2026-02-16 03:32:59 +0500

    Add files via upload

--- mars_bot.py
+++ mars_bot.py
@@ -1,23 +1,39 @@
 import discord
+import json
 import os # default module
 from dotenv import load_dotenv
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
-#nessesary factors, will cause death if uncontrolled. Water, energy and happyness
-nes={"wt":100, "en":100, "hp":100}
-#secondary factors, will make game really hard is uncontrolled. Pesourses, poison and food
-sec={"rs":100, "ps":25, "fd":100}
+class User:
+    #nessesary factors, will cause death if uncontrolled. Water, energy and happyness
+    nes={"wt":100, "en":100, "hp":100}
+    #secondary factors, will make game really hard is uncontrolled. Pesourses, poison and food
+    sec={"rs":100, "ps":25, "fd":100}
+    #adress of a thread where game is going and ID of user
+    thread=0
+    id = 0
+
+events={}
 
 @bot.event
 async def on_ready():
+    global events
+    with open("events.json", "r", encoding="utf-8") as f:
+        events = json.load(f)
     print(f"{bot.user} is ready and online!")
-
+    
 @bot.slash_command(name="newgame", description="start the game (ATTENTION: the old one will be deleted)")
 async def new_game(ctx: discord.ApplicationContext):
-    message = await ctx.send(f"The new game thread for {ctx.author} was created")
-    thread = (await message.create_thread(name=f"{ctx.author}'s game", auto_archive_duration=60))
+    global events
+    mes = (await ctx.send(f"Creating game thread for {ctx.author}"))
+    thread = (await mes.create_thread(name=f"{ctx.author}'s game", auto_archive_duration=60))
+    await thread.send(f"The new game thread for {ctx.author} was created")
     await thread.send("Человечество наконец то начало распространяться за пределы Земли. \nНадежда-1 является одной из первых колоний на Марсе")
-    await thread.send("<:energy:1471857321511747737>")
+    await thread.send(events["ru"]["0"]["text"])
+
+@bot.slash_command(name="save", description="save your game progress.")
+async def save(ctx: discord.ApplicationContext):
+    await ctx.send("Сохраняем")
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
